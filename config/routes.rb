3002d07f066f53ce95985399cbe86c0e9b38342d
Nov-Is <jza80.na2@gmail.com>
2024-09-07 16:47:43 +0900

    Postコントローラ作成、サインアウト、ヘッダー実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :posts, only: %i[show new]
+  root 'posts#index'
   devise_for :users
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   resources :tasks
